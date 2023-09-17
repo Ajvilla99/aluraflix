90b45b3e03b23199c83355406c39737f9706c371
Abrahan <ajvilla1999@gmail.com>
2023-09-16 23:56:29 -0500

    commit save

--- src/App.js
+++ src/App.js
@@ -5,7 +5,6 @@ import MediaUpload from './pages/MediaUpload';
 import Page404 from './pages/Page404';
 import Header from './components/Header'
 import NewCategory from './pages/NewCategory';
-import Prueba from './pages/Prueba'
 
 
 function App() {
@@ -16,7 +15,6 @@ function App() {
           <Route path='/' element={<Home/>}/>
           <Route path='/upload' element={<MediaUpload />}/>
           <Route path='/newCategory' element={<NewCategory />}/>
-          <Route path='/prueba' element={<Prueba />}/>
           <Route path='*' element={<Page404 />} />
       </Routes>
     </Router>
